8fc518e7b246b58fc5fe45da72c0c5ee586f68c8
Ayşe YALÇINKAYA <ayse_ykaya@hotmail.com>
2022-04-24 21:56:58 +0300

    Create operation classes and add functions

---
+++ app/src/main/java/com/ateam/travelguide/database/ImageOperation.kt
@@ -0,0 +1,84 @@
+package com.ateam.travelguide.database
+
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import com.ateam.travelguide.model.Image
+import com.ateam.travelguide.util.Constant.DATABASE_NAME
+import com.ateam.travelguide.util.Constant.ID
+import com.ateam.travelguide.util.Constant.IMAGE_LOCATION_ID
+import com.ateam.travelguide.util.Constant.IMAGE_TABLE
+import com.ateam.travelguide.util.Constant.IMAGE_URI
+
+class ImageOperation(context: Context) {
+    private var travelGuideDatabase: SQLiteDatabase? = null
+    private var dbOpenHelper: DatabaseOpenHelper = DatabaseOpenHelper(context,
+        DATABASE_NAME, null, 1)
+
+    private fun open() {
+        travelGuideDatabase = dbOpenHelper.writableDatabase
+    }
+
+    private fun close() {
+        if (travelGuideDatabase != null && travelGuideDatabase!!.isOpen) {
+            travelGuideDatabase!!.close()
+        }
+    }
+    fun addImage(image : Image) {
+        val cv = ContentValues()
+        cv.put(IMAGE_URI, image.uri)
+        cv.put(IMAGE_LOCATION_ID, image.locationId)
+
+        open()
+        travelGuideDatabase!!.insert(IMAGE_TABLE, null, cv)
+        close()
+    }
+    private fun getAllImageQuery(locationId: Int): Cursor { //for LocationDetailFragment
+        val query = "SELECT * FROM $IMAGE_TABLE WHERE $IMAGE_LOCATION_ID = ?"
+        return travelGuideDatabase!!.rawQuery(query, arrayOf(locationId.toString()))
+    }
+
+    @SuppressLint("Range")
+    fun getAllImage(locationId: Int): ArrayList<Image> {
+        val imageList = ArrayList<Image>()
+
+        open()
+        val c: Cursor = getAllImageQuery(locationId)
+
+        if (c.moveToFirst()) {
+            do {
+                val image = Image(
+                    id = c.getInt(0),
+                    uri = c.getString(c.getColumnIndex(IMAGE_URI)),
+                    locationId = c.getInt(c.getColumnIndex(IMAGE_LOCATION_ID))
+                )
+                imageList.add(image)
+            } while (c.moveToNext())
+        }
+
+        close()
+        return imageList
+    }
+    private fun getFirstImageQuery(): Cursor { //get first
+        val query = "SELECT * FROM $IMAGE_TABLE WHERE $ID =0"
+        return travelGuideDatabase!!.rawQuery(query,null)
+    }
+
+    @SuppressLint("Range")
+    fun getFirstImage(): Image? {
+        var image: Image? = null
+        open()
+        val c: Cursor = getFirstImageQuery()
+        while (c.moveToNext()) {
+            image = Image(
+                id = c.getInt(0),
+                uri = c.getString(c.getColumnIndex(IMAGE_URI)),
+                locationId = c.getInt(c.getColumnIndex(IMAGE_LOCATION_ID))
+            )
+        }
+        close()
+        return image
+    }
+}
---
+++ app/src/main/java/com/ateam/travelguide/database/VisitHistoryOperation.kt
@@ -0,0 +1,74 @@
+package com.ateam.travelguide.database
+
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import com.ateam.travelguide.model.VisitHistory
+import com.ateam.travelguide.util.Constant.DATABASE_NAME
+import com.ateam.travelguide.util.Constant.DAY
+import com.ateam.travelguide.util.Constant.HISTORY_LOCATION_ID
+import com.ateam.travelguide.util.Constant.HISTORY_LONG_DESCRIPTION
+import com.ateam.travelguide.util.Constant.MONTH
+import com.ateam.travelguide.util.Constant.VISIT_HISTORY_TABLE
+import com.ateam.travelguide.util.Constant.YEAR
+
+class VisitHistoryOperation(context: Context) {
+    private var travelGuideDatabase: SQLiteDatabase? = null
+    private var dbOpenHelper: DatabaseOpenHelper = DatabaseOpenHelper(context,
+        DATABASE_NAME, null, 1)
+
+    private fun open() {
+        travelGuideDatabase = dbOpenHelper.writableDatabase
+    }
+
+    private fun close() {
+        if (travelGuideDatabase != null && travelGuideDatabase!!.isOpen) {
+            travelGuideDatabase!!.close()
+        }
+    }
+
+    fun addVisitHistory(visitHistory : VisitHistory) {
+        val cv = ContentValues()
+        cv.put(YEAR, visitHistory.year)
+        cv.put(MONTH, visitHistory.month)
+        cv.put(DAY, visitHistory.day)
+        cv.put(HISTORY_LONG_DESCRIPTION, visitHistory.longDescription)
+        cv.put(HISTORY_LOCATION_ID, visitHistory.history_location_id)
+
+        open()
+        travelGuideDatabase!!.insert(VISIT_HISTORY_TABLE, null, cv)
+        close()
+    }
+
+    private fun getAllVisitHistoryQuery(locationId : Int): Cursor {
+        val query = "SELECT * FROM $VISIT_HISTORY_TABLE WHERE $HISTORY_LOCATION_ID = ?"
+        return travelGuideDatabase!!.rawQuery(query, arrayOf(locationId.toString()))
+    }
+
+    @SuppressLint("Range")
+    fun getAllVisitHistory(locationId : Int): ArrayList<VisitHistory> {
+        val visitHistoryList = ArrayList<VisitHistory>()
+
+        open()
+        val c: Cursor = getAllVisitHistoryQuery(locationId)
+
+        if (c.moveToFirst()) {
+            do {
+                val visitHistory = VisitHistory(
+                    id = c.getInt(0),
+                    year = c.getInt(c.getColumnIndex(YEAR)),
+                    month = c.getInt(c.getColumnIndex(MONTH)),
+                    day = c.getInt(c.getColumnIndex(DAY)),
+                    longDescription = c.getString(c.getColumnIndex(HISTORY_LONG_DESCRIPTION)),
+                    history_location_id = c.getInt(c.getColumnIndex(HISTORY_LOCATION_ID))
+                )
+                visitHistoryList.add(visitHistory)
+            } while (c.moveToNext())
+        }
+
+        close()
+        return visitHistoryList
+    }
+}
